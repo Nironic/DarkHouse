5cdd30d4bfbe5b984872dacdffa5c5ede22bfe7a
Nironic <nironicgod@mail.ru>
2022-07-11 20:35:40 +0300

    edfsdsdf

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
-﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello World");
+﻿using SFML;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+using SFML.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+RenderWindow window = new RenderWindow(new VideoMode(200,200), "test");
+CircleShape cs = new CircleShape(100.0f);
+cs.FillColor = Color.Green;
+window.SetActive();
+while (true)
+{
+    window.Clear();
+    window.DispatchEvents();
+    window.Draw(cs);
+    window.Display();
+}
