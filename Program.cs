3dcde5e38715a4cf449cdf9a054b6ce4d1e60b3f
Nironic <nironicgod@mail.ru>
2022-07-13 23:44:24 +0300

    rer

--- Program.cs
+++ Program.cs
@@ -13,10 +13,27 @@ var t = VideoMode.DesktopMode;
 uint x = t.Width;
 uint y = t.Height;
 
-RenderWindow window = new RenderWindow(new VideoMode(x, y), "test", Styles.Fullscreen);
+RenderWindow window = new RenderWindow(new VideoMode(x, y), "DarkHouse", Styles.Fullscreen);
 window.SetActive();
-while (true)
+
+Music loading_mus = new Music("res/start/music/autors.mp3");
+loading_mus.Play();
+
+///Video render
+Sprite loading = new Sprite (new Texture("res/start/autors.png"));
+var x_load = (x / 2) - 300;
+var y_load = (y / 2) - 90;
+Vector2f loading1 = new Vector2f(x_load, y_load);
+loading.Position = loading1;
+///
+
+
+window.SetFramerateLimit(120);
+
+
+while (window.HasFocus())
 {
     window.Clear();
+    window.Draw(loading);
     window.Display();
 }
